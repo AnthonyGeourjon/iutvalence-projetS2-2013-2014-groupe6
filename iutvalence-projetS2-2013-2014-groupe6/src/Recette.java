336b2df900cb7e48076ab22f898e9e408a6264d8
AnthonyGeourjon <anthony.geourjon@iut-valence.fr>
2014-05-09 10:59:39 +0200

    Ecriture de classe  recette à partir du code généré

--- iutvalence-projetS2-2013-2014-groupe6/src/Recette.java
+++ iutvalence-projetS2-2013-2014-groupe6/src/Recette.java
@@ -1,4 +1,31 @@
+import java.util.HashSet;
 
-public class Recette {
+public class Recette
+{
+
+	private static HashSet<Recette> toutesLesRecettesDisponibles;
+
+	private String nom;
+
+	private HashSet<Aliment> alimentsNecessaires;
+
+	public Recette(Aliment[] alimentsNecessaires, String nom)
+
+	{
+		this.nom = nom;
+		this.alimentsNecessaires=new HashSet<>();
+		
+		for (Aliment alimentCourant : alimentsNecessaires)
+		{
+			this.alimentsNecessaires.add(alimentCourant);
+		}
+		
+		this.toutesLesRecettesDisponibles.add(this);
+	}
+
+	public HashSet<Recette> obtenirToutesLesRecettesDisponibles()
+	{
+		return toutesLesRecettesDisponibles;
+	}
 
 }
