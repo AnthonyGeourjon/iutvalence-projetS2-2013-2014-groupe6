06ba22a61034b78ff344384e537c1cb06ed16885
AnthonyGeourjon <anthony.geourjon@iut-valence.fr>
2014-05-09 11:04:09 +0200

    Code + Javadoc de recette.

--- iutvalence-projetS2-2013-2014-groupe6/src/Recette.java
+++ iutvalence-projetS2-2013-2014-groupe6/src/Recette.java
@@ -3,26 +3,47 @@ import java.util.HashSet;
 public class Recette
 {
 
+	// Ajouter des variables "Qualité" et "difficulté"
+
+	/**
+	 * Ensemble des recettes enregistrés.
+	 */
 	private static HashSet<Recette> toutesLesRecettesDisponibles;
 
+	/**
+	 * Nom de la recette 
+	 */
 	private String nom;
 
+	/**
+	 * Ensemble des aliments nécessaires à sa confection 
+	 */
 	private HashSet<Aliment> alimentsNecessaires;
 
+	/**
+	 * @param alimentsNecessaires aliments nécessaires à sa confection 
+	 * @param nom nom de la recette 
+	 */
 	public Recette(Aliment[] alimentsNecessaires, String nom)
 
 	{
 		this.nom = nom;
-		this.alimentsNecessaires=new HashSet<>();
-		
+		this.alimentsNecessaires = new HashSet<>();
+
 		for (Aliment alimentCourant : alimentsNecessaires)
 		{
 			this.alimentsNecessaires.add(alimentCourant);
 		}
-		
-		this.toutesLesRecettesDisponibles.add(this);
+
+		Recette.toutesLesRecettesDisponibles.add(this);
+
+		// TODO Verifier si la recette n'est pas deja presente
+
 	}
 
+	/**
+	 * @return l'ensemble des recettes disponibles
+	 */
 	public HashSet<Recette> obtenirToutesLesRecettesDisponibles()
 	{
 		return toutesLesRecettesDisponibles;
