9efb6176237897e2b8232afd29410afd61d6098c
AnthonyGeourjon <anthony.geourjon@iut-valence.fr>
2014-05-09 13:36:30 +0200

    code

--- iutvalence-projetS2-2013-2014-groupe6/src/Aliment.java
+++ iutvalence-projetS2-2013-2014-groupe6/src/Aliment.java
@@ -1,9 +1,11 @@
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashSet;
 
 public class Aliment
 {
+	private static HashSet<Aliment> ensembleDesAlimentsConnus;
 
 	private String nom;
 
@@ -15,23 +17,36 @@ public class Aliment
 
 	private Integer prix;
 
+	public Aliment(Date datePeremption, String nomAliment, Integer quantiteInitiale, Integer nombreDExemplaireInitial,
+			Integer prix)
+	{
+		this.datePeremption = datePeremption;
+		this.nom = nomAliment;
+		this.quantite = quantiteInitiale;
+		this.nombreDExemplaire = nombreDExemplaireInitial;
+
+		Aliment.ensembleDesAlimentsConnus.add(this);
+
+	}
+
 	public String obtenirNom()
 	{
-		return null;
+		return this.nom;
 	}
 
 	public Date obtenirDateDePeremption()
 	{
-		return null;
-	}
-
-	public Aliment(Date datePeremption, String nomAliment, Integer quantiteInitiale, Integer nombreDExemplaireInitial)
-	{
-
+		return this.datePeremption;
 	}
 
 	public void changerNombreDExemplaire(Integer variation)
 	{
+		this.nombreDExemplaire += variation;
+	}
+
+	public Integer obtenirPrix()
+	{
+		return this.prix;
 	}
 
 }
--- iutvalence-projetS2-2013-2014-groupe6/src/Frigo.java
+++ iutvalence-projetS2-2013-2014-groupe6/src/Frigo.java
@@ -4,13 +4,18 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-
 public class Frigo
 {
 
 	private HashSet<Aliment> alimentsDuFrigo;
 	
-	
+	private HashSet<Recette> recettesDisponibles;
+
+	public Frigo()
+	{
+		this.alimentsDuFrigo = new HashSet<>();
+	}
+
 	public void insererAliment(Aliment alimentAAjouter)
 	{
 		this.alimentsDuFrigo.add(alimentAAjouter);
@@ -18,25 +23,26 @@ public class Frigo
 
 	public void supprimerAliment(Aliment alimentASupprimer) throws alimentsInexistantException
 	{
-		if(!this.alimentsDuFrigo.remove(alimentASupprimer))
+		if (!this.alimentsDuFrigo.remove(alimentASupprimer))
 			throw new alimentsInexistantException();
 	}
 
 	public HashSet<Aliment> alimentsPerimes()
 	{
 		HashSet<Aliment> alimentsPerimes = new HashSet<>();
-		
-		Object[] lesAliments = this.alimentsDuFrigo.toArray();
-		
-		for( Object alimentCourant : lesAliments)
-		{
-			if((Aliment)alimentCourant).obtenirDateDePeremption()<=
-		}
-		
-	}
 
-	public Frigo()
-	{
+		Object[] lesAliments = this.alimentsDuFrigo.toArray();
+
+		for (Object alimentCourant : lesAliments)
+		{
+
+			if (((Aliment) alimentCourant).obtenirDateDePeremption().before(new Date()))
+			{
+				alimentsPerimes.add((Aliment) alimentCourant);
+			}
+		}
+
+		return alimentsPerimes;
 
 	}
 
