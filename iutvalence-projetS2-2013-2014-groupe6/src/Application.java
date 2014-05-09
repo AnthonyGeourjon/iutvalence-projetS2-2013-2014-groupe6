1eb1d7b73bb4932f6dfa8cc074747ab2fa2ccdd5
AnthonyGeourjon <anthony.geourjon@iut-valence.fr>
2014-05-09 11:47:08 +0200

    Remarque sur le projet en commentaire

--- iutvalence-projetS2-2013-2014-groupe6/src/Application.java
+++ iutvalence-projetS2-2013-2014-groupe6/src/Application.java
@@ -1,7 +1,7 @@
 public class Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Reprise de l'application après l'avoir quitté. -> Enregistrement des données (BDD, fichiers ...)
 
 	}
 
