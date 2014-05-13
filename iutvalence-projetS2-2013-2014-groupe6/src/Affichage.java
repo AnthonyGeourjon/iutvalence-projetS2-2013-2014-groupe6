5b3fd21d4bcb4e5ff22a7723b4e31a71eaee3293
AnthonyGeourjon <anthony.geourjon@iut-valence.fr>
2014-05-13 20:37:55 +0200

    Classes d'affichage abstraite et console

---
+++ iutvalence-projetS2-2013-2014-groupe6/src/Affichage.java
@@ -0,0 +1,11 @@
+
+public interface Affichage
+{
+	public abstract void afficherLeMenu();
+	
+	public abstract void demanderUnChoix();
+	
+	public abstract void afficherLeNomDeLApplication();
+
+	public abstract void afficherSousMenu();
+}
---
+++ iutvalence-projetS2-2013-2014-groupe6/src/AffichageConsole.java
@@ -0,0 +1,32 @@
+public class AffichageConsole implements Affichage
+{
+	@Override
+	public void afficherLeNomDeLApplication()
+	{
+		System.out.println("Le couteau suisse de l'étudiant :\n\n");
+	}
+
+	@Override
+	public void afficherLeMenu()
+	{
+
+		System.out.println("Menu principal : \n\n1-Module Frigo, \n0-Quitter.\n");
+
+	}
+
+	@Override
+	public void demanderUnChoix()
+	{
+		System.out.println("Veuillez saisir votre choix :");
+
+	}
+	
+	@Override
+	public void afficherSousMenu()
+	{
+		System.out
+		.println("Module Gestion de frigo : \n1-Inserer un aliment au frigo, \n2-Supprimer un aliment du frigo, \n3-Afficher les aliments périmés\n4-Supprimer les aliments perimés, \n5-Afficher le frigo\n0-Menu prinicpal");
+
+		
+	}
+}
