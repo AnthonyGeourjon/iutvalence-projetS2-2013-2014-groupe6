68a512b48964bf45dcbc1b0c77e6751a8169dc1e
AnthonyGeourjon <anthony.geourjon@iut-valence.fr>
2014-05-13 20:33:32 +0200

    Adaptation au nouveau couteau etudiant

--- iutvalence-projetS2-2013-2014-groupe6/src/Application.java
+++ iutvalence-projetS2-2013-2014-groupe6/src/Application.java
@@ -2,7 +2,8 @@ public class Application {
 
 	public static void main(String[] args) {
 		
-		couteauSuisse unCouteauSuisse = new couteauSuisse();
+		couteauSuisse unCouteauSuisse = new couteauSuisse(new InteractionClavier(),new AffichageConsole(), new Frigo());
+		
 		
 		unCouteauSuisse.utiliserLeCouteauSuisse();
 
