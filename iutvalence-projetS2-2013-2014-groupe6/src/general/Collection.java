93958155c422135590c8e94d5879b54865f0a84e
Anthony <Anthony@172.16.11.253>
2014-05-22 19:27:14 +0200

    Juste une idéepour eviter de reinventer la rouea chaque fois

---
+++ iutvalence-projetS2-2013-2014-groupe6/src/general/Collection.java
@@ -0,0 +1,23 @@
+// Juste une idée pour ameliorer
+
+
+package general;
+
+import general.Collection;
+
+public class Collection<Type>
+{
+	private Type[] laCollection;
+	
+	private final Integer nombreMaxDElement;
+	
+	public Collection(Integer nombreDElement)
+	{
+		this.nombreMaxDElement=nombreDElement;
+		this.laCollection=(Type[])new Object[this.nombreMaxDElement];
+	}
+	
+	public void ajouter
+	
+	
+}
